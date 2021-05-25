4ec15f441fa53c548765d588cfdb7140f3e38867
INDRIAN <indrian16@outlook.co.id>
2021-05-25 19:32:57 +0800

    add splash screen

---
+++ app/src/main/java/io/indrian/whatmovies/ui/splash/SplashActivity.kt
@@ -0,0 +1,43 @@
+package io.indrian.whatmovies.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import io.indrian.whatmovies.BuildConfig
+import io.indrian.whatmovies.databinding.ActivitySplashBinding
+import io.indrian.whatmovies.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private var _binding: ActivitySplashBinding? = null
+    private val binding: ActivitySplashBinding get() = _binding!!
+
+    private val runnable = Runnable {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+        finishAffinity()
+    }
+    private val handler = Handler(Looper.getMainLooper())
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvVersion.text = "v${BuildConfig.VERSION_NAME}"
+
+        handler.postDelayed(runnable, 2000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+        handler.removeCallbacks(runnable)
+        handler.removeCallbacksAndMessages(null)
+    }
+}
