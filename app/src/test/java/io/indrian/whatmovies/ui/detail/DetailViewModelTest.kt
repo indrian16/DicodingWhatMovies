e875078912a9349c57adb9686a161ddcccba33b6
INDRIAN <indrian16@outlook.co.id>
2021-06-29 15:18:15 +0800

    update DetailViewModel

--- app/src/test/java/io/indrian/whatmovies/ui/detail/DetailViewModelTest.kt
+++ app/src/test/java/io/indrian/whatmovies/ui/detail/DetailViewModelTest.kt
@@ -1,48 +1,74 @@
 package io.indrian.whatmovies.ui.detail
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import io.indrian.whatmovies.data.DummyData
+import io.indrian.whatmovies.data.models.Movie
+import io.indrian.whatmovies.data.models.TVShow
 import io.indrian.whatmovies.data.repositories.Repository
-import io.indrian.whatmovies.utils.CommonState
+import io.indrian.whatmovies.vo.Resource
 import io.mockk.MockKAnnotations
+import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 
 class DetailViewModelTest {
 
-    @RelaxedMockK
-    lateinit var repository: Repository
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var viewModel: DetailViewModel
 
-    @ExperimentalCoroutinesApi
+    private val dummyMovieId = DummyData.getMovies()[0].id
+    private val dummyMovie = Resource.success(DummyData.getDetailMovies(dummyMovieId))
+
+    private val dummyTVShowId = DummyData.getTVShows()[0].id
+    private val dummyTVShow = Resource.success(DummyData.getDetailTVShow(dummyTVShowId))
+
+    @RelaxedMockK
+    private lateinit var repository: Repository
+
+    @RelaxedMockK
+    private lateinit var movieObserver: Observer<Resource<Movie>>
+
+    @RelaxedMockK
+    private lateinit var tvShowObserver: Observer<Resource<TVShow>>
+
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
         viewModel = DetailViewModel(repository)
     }
 
-//    @DelicateCoroutinesApi
-//    @ExperimentalCoroutinesApi
-//    @Test
-//    fun getDetailMovies() = coroutinesTestRule.testDispatcher.runBlockingTest {
-//        GlobalScope.launch {
-//            viewModel.getDetailMovies(12345)
-//        }
-//        viewModel.stateDetailMovie.getOrAwaitValue().shouldBe(CommonState.Loading)
-//    }
-//
-//    @DelicateCoroutinesApi
-//    @ExperimentalCoroutinesApi
-//    @Test
-//    fun getDetailTVShow() = coroutinesTestRule.testDispatcher.runBlockingTest {
-//        GlobalScope.launch {
-//            viewModel.getDetailTVShow(54321)
-//        }
-//        viewModel.stateDetailTVShow.getOrAwaitValue().shouldBe(CommonState.Loading)
-//    }
+    @After
+    fun tearDown() = unmockkAll()
+
+    @Test
+    fun getDetailMovie() {
+        val liveMovie = MutableLiveData<Resource<Movie>>()
+        liveMovie.value = dummyMovie
+        every { repository.getDetailMovies(dummyMovieId) } returns liveMovie
+
+        viewModel.getDetailMovies(dummyMovieId)
+        viewModel.getDetailMovies(dummyMovieId).observeForever(movieObserver)
+        verify { movieObserver.onChanged(dummyMovie) }
+    }
+
+    @Test
+    fun getTVShowDetail() {
+        val liveTVShow = MutableLiveData<Resource<TVShow>>()
+        liveTVShow.value = dummyTVShow
+        every { repository.getDetailTVShow(dummyTVShowId) } returns liveTVShow
+
+        viewModel.getDetailTVShow(dummyTVShowId)
+        viewModel.getDetailTVShow(dummyTVShowId).observeForever(tvShowObserver)
+        verify { tvShowObserver.onChanged(dummyTVShow) }
+    }
 }
