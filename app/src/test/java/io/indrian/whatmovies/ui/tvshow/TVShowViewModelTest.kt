ffce83845ddec0fcf42b8047a25584028c312504
INDRIAN <indrian16@outlook.co.id>
2021-06-29 15:02:59 +0800

    update TVShowViewModelTest

--- app/src/test/java/io/indrian/whatmovies/ui/tvshow/TVShowViewModelTest.kt
+++ app/src/test/java/io/indrian/whatmovies/ui/tvshow/TVShowViewModelTest.kt
@@ -1,28 +1,40 @@
 package io.indrian.whatmovies.ui.tvshow
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.paging.PagedList
+import io.indrian.whatmovies.data.models.TVShow
 import io.indrian.whatmovies.data.repositories.Repository
-import io.indrian.whatmovies.utils.CoroutinesTestRule
+import io.indrian.whatmovies.vo.Resource
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
+import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.unmockkAll
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class TVShowViewModelTest {
 
-    @Rule
-    @JvmField
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
-
-    @ExperimentalCoroutinesApi
     @get:Rule
-    val coroutinesTestRule = CoroutinesTestRule()
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @RelaxedMockK
     lateinit var repository: Repository
 
     private lateinit var viewModel: TVShowViewModel
+
+    @RelaxedMockK
+    private lateinit var observer: Observer<Resource<PagedList<TVShow>>>
+
+    @RelaxedMockK
+    private lateinit var pagedList: PagedList<TVShow>
 
     @ExperimentalCoroutinesApi
     @Before
@@ -30,14 +42,26 @@ class TVShowViewModelTest {
         MockKAnnotations.init(this)
         viewModel = TVShowViewModel(repository)
     }
-//
-//    @DelicateCoroutinesApi
-//    @ExperimentalCoroutinesApi
-//    @Test
-//    fun getTVShows() = coroutinesTestRule.testDispatcher.runBlockingTest {
-//        GlobalScope.launch {
-//            viewModel.getTVShows()
-//        }
-//        viewModel.tvShowState.getOrAwaitValue().shouldBe(CommonState.Loading)
-//    }
+
+    @After
+    fun tearDown() = unmockkAll()
+
+    @Test
+    fun getTVShows() {
+        val dummyTVShows = Resource.success(pagedList)
+        every { dummyTVShows.data?.size } returns 15
+
+        val liveTVShows = MutableLiveData<Resource<PagedList<TVShow>>>()
+        liveTVShows.value = dummyTVShows
+
+        every { repository.getTVShows() } returns liveTVShows
+        val movies = viewModel.getTVShows().value?.data
+        verify { repository.getTVShows() }
+
+        movies.shouldNotBeNull()
+        movies.size.shouldBe(15)
+
+        viewModel.getTVShows().observeForever(observer)
+        verify { observer.onChanged(dummyTVShows) }
+    }
 }
